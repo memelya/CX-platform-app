0f630c0bc2f54929e85daa81897510f3b5bece5f
memelya <maxemel223@gmail.com>
2026-05-09 05:19:19 +0000

    fix: remove crossorigin attr for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/CX-platform-/',
   root: '.',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
+    target: 'esnext',
   },
+  esbuild: { target: 'esnext' },
+  plugins: [
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml: {
+        order: 'post',
+        handler(html) {
+          return html.replace(/crossorigin\s*/g, '');
+        },
+      },
+    },
+  ],
 });
